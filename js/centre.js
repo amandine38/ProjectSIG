d3cd0427dd2443dc617df35498029573f21373bc
Ingrid Kini <ingrid.kini@aikinggroup.tech>
2024-09-11 14:18:05 +0100

    SigProject

---
+++ js/centre.js
@@ -0,0 +1,23 @@
+var centres = [
+    {
+        "name": "Centre d'Accueil des Réfugiés de Ouagadougou",
+        "lat": 12.364646,
+        "lng": -1.533879,
+        "description": "Centre offrant des services d'accueil et de soutien aux réfugiés.",
+        "website": "https://www.unhcr.org/fr/burkina-faso.html"
+    },
+    {
+        "name": "ONG Terre des hommes, Ouahigouya",
+        "lat": 13.569526,
+        "lng": -2.416818,
+        "description": "ONG internationale spécialisée dans la protection des enfants.",
+        "website": "https://www.tdh.ch/fr/nos-interventions/burkina-faso"
+    },
+    {
+        "name": "Centre Croix-Rouge, Bobo-Dioulasso",
+        "lat": 11.17714,
+        "lng": -4.2979,
+        "description": "Centre d'assistance humanitaire de la Croix-Rouge.",
+        "website": "https://www.croix-rouge.fr"
+    }
+];
---
+++ js/ong.js
@@ -0,0 +1,16 @@
+var ongs = [
+    {
+        "name": "International Rescue Committee (IRC) - Ouagadougou",
+        "lat": 12.364646, // Coordonnées à ajuster selon le site réel
+        "lng": -1.533879,
+        "description": "Organisation humanitaire internationale aidant les personnes déplacées par les conflits.",
+        "website": "https://www.rescue.org/country/burkina-faso"
+    },
+    {
+        "name": "Action Contre la Faim (ACF) - Ouahigouya",
+        "lat": 13.569526, // Coordonnées à ajuster selon le site réel
+        "lng": -2.416818,
+        "description": "ONG internationale luttant contre la faim et la malnutrition.",
+        "website": "https://www.actioncontrelafaim.org/fr/pays/afrique/burkina-faso/"
+    }
+];
