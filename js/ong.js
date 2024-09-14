3213a7c630bfea6fcb6c1bfdf2ddf9ab9214d8b5
Ingrid Kini <ingrid.kini@aikinggroup.tech>
2024-09-14 19:01:46 +0100

    update

--- js/ong.js
+++ js/ong.js
@@ -12,5 +12,186 @@ var ongs = [
         "lng": -2.416818,
         "description": "ONG internationale luttant contre la faim et la malnutrition.",
         "website": "https://www.actioncontrelafaim.org/fr/pays/afrique/burkina-faso/"
-    }
+    },
+      
+  {
+    "name": "Direction provinciale tuy de la solidarité et l'action humanitaire,de la réconciliation genre et de la famille",
+    "lat": 12.39878267153052, 
+    "lng": -2.0920489641961657,
+    "responsable": "",
+    "adresse": " 9F4F+28M, rue 7.06, Kamsonghin, Ouagadougou",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Direction générale de la promotion de la femme et du genre",
+    "lat": 12.301820366784982,  
+    "lng": -1.752902912121541,
+    "responsable": "",
+    "adresse": "8F33+Q9, Sandogo, Ouagadougou",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Direction Provinciale de la Femme de la Solidarité Nationale et de la Famille",
+    "lat": 12.37218951712814, 
+    "lng": -2.0077872981760647,
+    "responsable": "",
+    "adresse": "8GCM+5CC, Réo",
+    "email": "",
+    "telephone": "+226 25445011",
+    "website": ""
+  },
+  {
+    "name": "DIRECTION PROVINCIALE DU MINISTERE DE LA FEMME DE LA SOLIDARITE NATIONALE ET DE L'ACTION SOCIALE KOUPELA",
+    "lat": 12.242262511088805,
+    "lng": -0.08507247293868056,
+    "responsable": "",
+    "adresse": "5JGP+CXF, Koupéla",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Ministère de la Promotion de la femme",
+    "lat": 12.377601744952875,  
+    "lng": -1.874804140234575, 
+    "responsable": "",
+    "adresse": "8FWF+3V7, Cité An II, Ouagadougou",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Action Sociale ouagadougou",
+    "lat": 12.333660790181067, 
+    "lng": -1.4429449755619805 ,
+    "responsable": "",
+    "adresse": "8FGF+J86, Patte d´oie, Ouagadougou",
+    "email": "",
+    "telephone": "+226 72258450",
+    "website": "http://www.action-sociale.gov.bf/"
+  },
+  {
+    "name": "irection régionale de l´action sociale Bobo-dioulassa",
+    "lat": 11.133762649142785,  
+    "lng": -4.090870140835541,
+    "responsable": "",
+    "adresse": "5P66+527, Bolomakoté, Bobo-Dioulasso",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Direction Provincial/ Action Sociale Bobo-Dioulasso",
+    "lat": 11.159915265159345, 
+    "lng": -4.120494041203465,
+    "responsable": "",
+    "adresse": "5P56+G98, Bolomakoté, Bobo-Dioulasso",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+//   {
+//     "name": "Direction Provinciale De L'Action Sociale Et De La Solidarité Nationale (DPASSN) Djibo",
+//     "lat": ,  
+//     "lng": ,
+//     "responsable": "",
+//     "adresse": "4929+7F9, Djibo",
+//     "email": "",
+//     "telephone": "+226 ",
+//     "website": ""
+//   },
+  {
+    "name": "Direction Régionale de l'Action Sociale et de la Solidarité Nationale du Centre-Nord",
+    "lat": 13.13118682415714, 
+    "lng": -0.9484262570348279,
+    "responsable": "",
+    "adresse": "3WW7+J5Q, Kaya",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Service action Sociale de tampouy",
+    "lat": 12.436573040737423,   
+    "lng": -1.414511485981811,
+    "responsable": "",
+    "adresse": "9CXG+78C, Secteur 16, Tampouy, Ouagadougou",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "DIRECTION PROVINCIALE DU MINISTERE DE LA FEMME ET DE L'ACTION SOCIALE DU CENTRE-EST",
+    "lat": 11.777101265997848, 
+    "lng": -0.25609114041162734,
+    "responsable": "",
+    "adresse": "QJM6+9CJ, Tenkodogo",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Centre d'écoute de l'action sociale Toma",
+    "lat": 12.770397561818813,  
+    "lng": -2.8732032383524233,
+    "responsable": "",
+    "adresse": "Q485+64, Toma",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Action Sociale Fada-Ngourma",
+    "lat": 12.183572110148582, 
+    "lng": 0.6036035242532174,
+    "responsable": "",
+    "adresse": "3975+CW2, Fada-Ngourma",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "ACTION SOCIALE DE DORI ",
+    "lat": 14.10324628061324,   
+    "lng": -0.1386850973693409,
+    "responsable": "",
+    "adresse": "4V22+26J, Yakouta",
+    "email": "",
+    "telephone": "+226 70524974",
+    "website": ""
+  },
+  {
+    "name": "Direction Provinciale de l'action Sociale du Ganzourgou",
+    "lat": 12.28157185063415, 
+    "lng": -0.401096598968061,
+    "responsable": "",
+    "adresse": "69WM+64P, Zorgo",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
+  {
+    "name": "Direction Provinciale De L'Action Sociale Du Zoundwéogo",
+    "lat": 11.749127254087595,   
+    "lng": -0.5929548063934792,
+    "responsable": "",
+    "adresse": "MW8M+9WH, Unnamed Road, Manga",
+    "email": "",
+    "telephone": "+226 25400033",
+    "website": ""
+  },
+  {
+    "name": "Direction Provinciale de l'Action Sociale des Balé (Boromo)",
+    "lat": 11.813970272393822, 
+    "lng": -2.9467432889581993,
+    "responsable": "",
+    "adresse": "P3RF+J3H, Boromo",
+    "email": "",
+    "telephone": "+226 ",
+    "website": ""
+  },
 ];
